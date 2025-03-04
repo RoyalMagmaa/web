0eda544613addd288f043fe36d1d15c276d00b13
RoyalMagmaa <106256283+RoyalMagmaa@users.noreply.github.com>
2025-03-04 14:49:11 +0100

    page createcompany

---
+++ app/views/auth/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Stageo</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div class="conteneur-auth flex-colonne centre-alignement">
+    <div class="auth centre-alignement">
+        <div class="texte-offre">
+            <img src="img/logo.png" alt="Logo" class="logo-login">
+            <h1>Connexion</h1>
+            <h2>Authentifiez-vous pour accéder à la plateforme</h2>
+        </div>
+        <div class="entree-auth">
+            <form id="formulaire-login" method="post">
+                <input class="email-entree" id="email-login" placeholder="Email" required type="text">
+                <input id="mdp-login" placeholder="Mot de passe" required type="password">
+                <button onclick="checkForm()" type="button">Connexion</button>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
