0a5d6b62cab6ba53abe46decb5589f8065d55ee8
RoyalMagmaa <106256283+RoyalMagmaa@users.noreply.github.com>
2025-03-04 15:28:21 +0100

    whishlist

---
+++ V1/wishlist.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Wishlist</title>
+    <link href="style.css" rel="stylesheet">
+    <script defer src="/script/jquery/jquery-3.7.1.js"></script>
+    <script defer src="script/components.js"></script>
+    <script defer src="script/input-validation.js"></script>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="navbar">
+                <a href="offres.php">Retour aux Offres</a>
+            </div>
+        </nav>
+    </header>
+    <div class="main-section">
+        <div class="header-section">
+            <h1 id="titre-offre">Votre Wishlist</h1>
+        </div>
+        <div class="sub-section" id="wishlist-container">
+            <!-- Annonces simulées -->
+            <div class="offre">
+                <h2>Stage Développeur Web</h2>
+                <p>Sur site</p>
+                <p>Lyon, Auvergne-Rhône-Alpes</p>
+            </div>
+            <div class="offre">
+                <h2>Stage Administrateur Réseau</h2>
+                <p>Sur site</p>
+                <p>Paris, Île-de-France</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
